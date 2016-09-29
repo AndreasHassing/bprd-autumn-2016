013e35cb41de93dc258377cae844260321a83d5f
AndreasHassing <andreas@famhassing.dk>
2016-09-29 21:43:39 +0200

    Clear up comments showing function return values

--- Assignments/4/BPRD-04-AndreasHassing-MarkusThomsen.fsx
+++ Assignments/4/BPRD-04-AndreasHassing-MarkusThomsen.fsx
@@ -16,12 +16,12 @@ open ParseAndRunHigher;;
 
 /// Exercise 6.1
 run (fromString @"let add x = let f y = x+y in f end
-in add 2 5 end");; // => HigherFun.value = Int 7
+in add 2 5 end");; // => Int 7
 
 run (fromString @"let add x = let f y = x+y in f end
 in let addtwo = add 2
     in addtwo 5 end
-end");; // => HigherFun.value = Int 7
+end");; // => Int 7
 
 // "the third one"
 // yes the output is expected, as the `x` defined
@@ -31,7 +31,7 @@ run (fromString @"let add x = let f y = x+y in f end
 in let addtwo = add 2
     in let x = 77 in addtwo 5 end
     end
-end");; // => HigherFun.value = Int 7
+end");; // => Int 7
 
 // "the last one"
 // in this one we get a closure back, that encapsulates
@@ -62,9 +62,9 @@ in add 2 end");; (* =>
 /// Exercise 6.3
 // We did this in exercise 6.2, as we thought the
 // functionality was needed all around to begin with.
-// Following are some examples of the functionality (working).
+// Following are some working examples of the functionality.
 run (fromString @"let add x = fun y -> x+y in add 2 5 end");;
-    // => HigherFun.value = Int 7
+    // => Int 7
 
 run (fromString @"let y = 22 in fun z -> z+y end");;
-    // => HigherFun.value = Clos ("z",Prim ("+",Var "z",Var "y"),[("y", Int 22)])
+    // => Clos ("z",Prim ("+",Var "z",Var "y"),[("y", Int 22)])
