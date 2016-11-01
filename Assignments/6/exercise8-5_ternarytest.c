dc00a43184276144f646673ea99f45b961d8c7f4
AndreasHassing <andreas@famhassing.dk>
2016-11-01 17:24:58 +0100

    More samples to test of ternary conditional expression

--- Assignments/6/exercise8-5_ternarytest.c
+++ Assignments/6/exercise8-5_ternarytest.c
@@ -1,7 +1,15 @@
 void main(int n) {
     int x;
-    x = 5 > 4 ? 5 : 7;
+    x =   5 > 4  ? 100 : -1;
     print x;
 
-    print 5 < 4 ? 6 : 3;
+    print 5 == 4 ?  50 : -1;
+
+    10 == 10 ? otherFunc() : 0; // calls otherFunc()
+    10 == 5  ? otherFunc() : 0; // 0, and nothing happens
+}
+
+int otherFunc() {
+    print 42;
+    return 1;
 }
